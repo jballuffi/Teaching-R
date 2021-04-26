1992dc65ffa4b1afc1e05bbb702ac708b1897c8e
Juliana Balluffi-Fry <jballuffi@gmail.com>
2021-04-25 23:52:12 -0600

    bulk data.table on ecological data

--- R/intro_to_datatable.R
+++ R/intro_to_datatable.R
@@ -80,39 +80,123 @@ MPG <- c[, .(mean(mpg), sd(mpg)), by = gear] #the period is needed!
 setnames(MPG, c("V1", "V2"), c("Meanmpg", "SDmpg"))
 
 
-#Example of a complete i, j, by----
+
+# Example of a complete 'with i, do j, by group' --------------------------
+
+#with 4-cylinder cars, count number of data, by gear
+c[cyl==4, .N, by = gear]
 
 #with gas guzzlers, calculate mean disp, by gear
 c[classification == "gas guzzler", mean(disp), by = gear]
 
-#next steps: 
-#working with multiple columns simultaneously
-#merge function
+
+
+# More advanced techniques ------------------------------------------------
+# Here we will be using some fabricated foraging data
+f <- fread("Input/foraging_data.csv")
+
+f[, .N]
+f[, unique(ID)]
+f[, unique(ID), by = habitat]
+f[, .N, by = ID]
+
+f[, mean(graze)]
+f[, mean(browse)]
+
+f[, unique(graze_unit)] #check out grazing units
+f[, unique(browse_unit)] #check out grazing units
+
+
+
+
+#units aren't the same, lets convert using data.table
+f[graze_unit=="kg/day", graze := graze*1000][, graze_unit := "g/day"]
+
+f[graze_unit=="kg/day", c('graze', 'graze_unit') := .(graze*1000, "g/day")]
+
+f[graze_unit=="kg/day"|browse_unit=="kg/day",
+  c('graze', 'browse', 'graze_unit', 'browse_unit') 
+  := .(graze*1000, browse*1000, "g/day", "g/day")]
+
+un <- grep("unit", names(f), value=TRUE)
+
+f[grepl("kg", list(un))]
+
+f[grepl("kg", graze_unit)|grepl("kg", browse_unit),
+  c('graze', 'browse', 'graze_unit', 'browse_unit') 
+  := .(graze*1000, browse*1000, "g/day", "g/day")]
+
+
+#create an idate
+f[, date := ymd(date)]
+
+#create a year column
+f[, year := year(idate)]
+
+f[, year2 := tstrsplit(idate, "-", keep=1)]
+
+
+f[, mean(browse), by = ID]
+f[, mean(graze), by = habitat]
+test<-f[, .(mean(graze), sd(graze)), by = c('ID', 'habitat', 'region')]
+setnames(test, c('V1', 'V2'), c('mean_graze', 'sd_graze'))
+
+f[, mean_graze := mean(graze), by = ID]
+
+#issue with this formate when plotting
+ggplot(f)+
+  geom_point(aes(x=date, y=graze))+
+  geom_point(aes(x=date, y=browse), color="blue3")
+
+#use the melt function
+# measure.vars = the columns you want to collapse into onw
+# value.name = what this new column will be called
+# variable.name = name of the column that categorizes them
+
+feed <- melt(f, measure.vars = c("browse", "graze"), value.name = "feedrate", variable.name = "foodtype")
+feed[, .N] #our new data.table has 400 rows, double before
+feed[foodtype=="graze", foodtype := "grass"]
+feed[, c("browse_unit", "graze_unit"):=NULL]
+
+ggplot(feed)+
+  geom_point(aes(x=date, y=feedrate, color=foodtype))
+
+
+
+
+rsq(lm(feedrate~foodtype + date, data=feed))
+
+coef(lm(feedrate~foodtype + date, data=feed))
+
+feed[, coef(lm(feedrate~foodtype + date))]
+feed[ID=="D", coef(lm(feedrate~foodtype + date))]
+feed[, coef(lm(feedrate~foodtype + date)), by=ID]
+mods<- feed[, data.table(t(coef(lm(feedrate~foodtype + date)))), by=ID]
+
+
+
+CNRSF <- function(yvar, xvar1, xvar2) {
+  # Make the model
+  model <- lm(yvar ~ xvar1 + xvar2)
+  # Transpose the coef of the model and cast as data.table
+  coefOut <- data.table(t(coef(model)))
+  # Transpose the standard error coef of the model and cast as data.table
+  secoefOut <- data.table(t(se.coef(model)))
+  #label the column name for the rsqOut
+  names(rsqOut)<-c("rsq")
+  # Add 'se-' prefix to standard error coef table
+  setnames(secoefOut, paste0('se-', colnames(secoefOut)))
+  # Return combined columns
+  return(data.table(coefOut, secoefOut, rsqOut))
+}
+
 #write your own function, or model, and run by group
 #lapply
-#melt 
 #dcast
-#tstrsplit
+
 
 
 #date time with lynx data ----
 
-#separate the date time column and keep date
-lynx[, Date := tstrsplit(datetime, " ", fixed = TRUE, keep = c(1))]
-
-#separate the datetime column and keep time
-lynx[, Time := tstrsplit(datetime, " ", fixed = TRUE, keep = c(2))]
-
-#separate the date column and keep year
-lynx[, Year := tstrsplit(Date, "-", fixed = TRUE, keep = c(1))]
-
-#reclassify the dates and times
-lynx[, idate := ymd(Date)] #create idate
-lynx[, itime := hms(Time)] #create itime
-lynx[, datetime := ymd_hms(datetime)] #create datetime
-lynx[, month := month(idate)] #create a month
 
 
-#now save this new version of the lynx data as an RDS file
-## this will maintain the date and time classifications when you load this data the next time
-saveRDS(lynx, "Input/lynxcleaned.rds")
